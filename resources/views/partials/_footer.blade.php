c4da92a163eccc6a7b9f5dc5a9d10dd548d5a29d
konyuka <michaelsaiba84@gmail.com>
2023-03-28 17:11:48 +0300

    footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -26,8 +26,8 @@
                      <div class="pxp-footer-address">
                         <p>1st Floor,Morningside Office Park, Ngong Road.<br>
                         P.O. Box 58622-00200 Nairobi, Kenya.</p>
-                        <a class="pxp-footer-header" href="tel:+254 707 111 777">+254 707 111 777</a>
-                        <p>sales@rochman-properties.co.ke</p>
+                        <a class="pxp-footer-header" href="tel:+254 707 111 777">+254 707 111 777</a> <br>
+                        <a class="pxp-footer-header" href="mailto:sales@rochman-properties.co.ke">sales@rochman-properties.co.ke</a>
                      </div>
                   </div>
             </div>
