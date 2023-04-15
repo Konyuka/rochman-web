536b0e4b71ee33b416168d8d28ae01220bcd91b0
konyuka <michaelsaiba84@gmail.com>
2023-04-15 12:59:07 +0300

    push

--- resources/views/partials/_message.blade.php
+++ resources/views/partials/_message.blade.php
@@ -24,6 +24,7 @@
 		<button type="button" class="close" data-dismiss="alert"><span>×</span><span class="sr-only">Close</span></button>
 		<span class="text-semibold">Errors!</span><br>
 		<ul>
+		
 			@foreach ($errors->all() as $error)
 				<li>{{ $error }}</li>
 			@endforeach
